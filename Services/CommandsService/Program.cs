bb977b318d25946934222368c2878ae59f7785ef
Bivash Thakur <bivashth8@gmail.com>
2026-05-24 02:06:02 +0530

    Command Service Changes

--- Services/CommandsService/Program.cs
+++ Services/CommandsService/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
